dbcbf4eec6bd4bffb8f51be50758ecc015a91203
Iain-Crowe <iaincrowe@knights.ucf.edu>
2022-12-19 21:13:27 -0500

    .

--- client/src/contexts/BuildPath.js
+++ client/src/contexts/BuildPath.js
@@ -2,7 +2,7 @@ const app_name = 'kalvin';
 
 export function basePath() {
     if (process.env.NODE_ENV === 'production') {
-        return 'ERROR: SERVER NOT SET UP';
+        return `https://${app_name}.herokuapp.com`;
     }
     else {
         return 'http://localhost:5000';
