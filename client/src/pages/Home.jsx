fd5655d1dffb0fd6bcad71538d7613abdee6b8b7
Iain-Crowe <iaincrowe@knights.ucf.edu>
2022-12-27 22:50:14 -0500

    Connected register endpoint to frontend, refactored homepage to be responsive for all screen sizes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { AiOutlineEdit, AiOutlineCloseCircle } from "react-icons/ai";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const Home = () => {
     const priority = ["!", "!!", "!!!"];
@@ -42,72 +43,188 @@ const Home = () => {
         },
     ];
 
+    const { screenSize, setScreenSize } = useStateContext();
+    const [medScreen, setMedScreen] = useState(false);
+    const [smallScreen, setSmallScreen] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => setScreenSize(window.innerWidth);
+
+        window.addEventListener("resize", handleResize);
+
+        handleResize();
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    useEffect(() => {
+        if (screenSize <= 1350) {
+            setMedScreen(true);
+        } else {
+            setMedScreen(false);
+        }
+
+        if (screenSize <= 768) {
+            setSmallScreen(true)
+        } else {
+            setSmallScreen(false)
+        }
+    }, [screenSize]);
+
     return (
-        <div className="flex justify-between p-4 gap-4 font-display">
-            <div className="flex border-2 border-gray-600 dark:border-gray-500 bg-white dark:bg-secondary-dark h-[82vh] w-2/3 rounded-lg drop-shadow-xl">
-                Profile
-            </div>
-            <div className="flex-row border-2 border-gray-600 dark:border-gray-500 bg-white dark:bg-secondary-dark h-[82vh] w-1/3 rounded-lg drop-shadow-xl overflow-y-auto scroll smooth">
-                <div className="flex py-4 px-6 text-black dark:text-gray-300 text-lg font-bold">
-                    <h2>Upcoming:</h2>
-                </div>
-                <div className="flex-row px-6">
-                    {items.map((item) => (
-                        <div
-                            key={item.index}
-                            className="flex-col justify-between w-[100%] p-4 mb-4 bg-main dark:bg-main-dark border-2 border-gray-600 dark:border-gray-500 rounded-xl drop-shadow-lg">
-                            <div className="flex-row">
-                                <span className="text-black dark:text-gray-300 text-md font-semibold w-11/12">
-                                    {item.title}
-                                    {" -"}
-                                </span>
-                                <span className="text-gray-700 dark:text-gray-400 text-sm line-clamp-2 mt-2 w-11/12">
-                                    {item.desc}
-                                </span>
-                                <div className="flex justify-between items-center mt-2 w-11/12">
-                                    <div className="">
-                                        <span className="text-gray-700 dark:text-white text-sm italic font-semibold bg-yellow-300/50 p-1 rounded-md">
-                                            {item.date}
-                                        </span>
-                                        <span className="text-gray-700 py-1 dark:text-gray-300 font-extrabold text-sm pl-2">
-                                            |
-                                        </span>
-                                        <span className="px-2 py-1 text-red-600 text-sm font-semibold">
-                                            {priority[item.priority]}
-                                        </span>
-                                    </div>
-                                    <div className="flex">
-                                        {item.tags.map((tag) => (
-                                            <div
-                                                key={tag.title}
-                                                className="mx-1 px-1 rounded-md"
-                                                style={{
-                                                    backgroundColor: tag.color,
-                                                }}>
-                                                <span className="text-white text-xs">
-                                                    {tag.title}
-                                                </span>
-                                            </div>
-                                        ))}
-                                    </div>
-                                </div>
+        <>
+            {medScreen ? (
+                <>
+                    <div className="flex-row justify-between p-4 gap-4 font-display min-h-min">
+                        <div className={`flex-row border-2 border-gray-600 dark:border-gray-500 bg-white dark:bg-secondary-dark h-56 w-full rounded-lg drop-shadow-xl overflow-y-auto ${smallScreen ? 'mt-16' : ''}`}>
+                            <div className="flex py-4 px-6 text-black dark:text-gray-300 text-lg font-bold">
+                                <h2>Upcoming:</h2>
                             </div>
-                            <div className="grid absolute top-0 right-2 h-full place-content-evenly text-xl text-black dark:text-gray-300 w-1/12">
-                                <button className="hover:bg-gray-400/50 dark:hover:bg-gray-600/50 p-2 rounded-full">
-                                    <AiOutlineEdit />
-                                </button>
-                                <button className="text-red-600 hover:bg-gray-400/50 dark:hover:bg-gray-600/50 p-2 rounded-full">
-                                    <AiOutlineCloseCircle />
+                            <div className="flex-row px-6">
+                                {items.map((item) => (
+                                    <div
+                                        key={item.index}
+                                        className="flex-col justify-between w-[100%] p-4 mb-4 bg-main dark:bg-main-dark border-2 border-gray-600 dark:border-gray-500 rounded-xl drop-shadow-lg">
+                                        <div className="flex-row">
+                                            <span className="text-black dark:text-gray-300 text-md font-semibold w-11/12">
+                                                {item.title}
+                                                {" -"}
+                                            </span>
+                                            <span className="text-gray-700 dark:text-gray-400 text-sm line-clamp-2 mt-2 w-11/12">
+                                                {item.desc}
+                                            </span>
+                                            <div className="flex justify-between items-center mt-2 w-11/12">
+                                                <div className="">
+                                                    <span className="text-gray-700 dark:text-white text-sm italic font-semibold bg-yellow-300/50 p-1 rounded-md">
+                                                        {item.date}
+                                                    </span>
+                                                    <span className="text-gray-700 py-1 dark:text-gray-300 font-extrabold text-sm pl-2">
+                                                        |
+                                                    </span>
+                                                    <span className="px-2 py-1 text-red-600 text-sm font-semibold">
+                                                        {
+                                                            priority[
+                                                                item.priority
+                                                            ]
+                                                        }
+                                                    </span>
+                                                </div>
+                                                <div className="flex">
+                                                    {item.tags.map((tag) => (
+                                                        <div
+                                                            key={tag.title}
+                                                            className="mx-1 px-1 rounded-md"
+                                                            style={{
+                                                                backgroundColor:
+                                                                    tag.color,
+                                                            }}>
+                                                            <span className="text-white text-xs">
+                                                                {tag.title}
+                                                            </span>
+                                                        </div>
+                                                    ))}
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="grid absolute top-0 right-2 h-full place-content-evenly text-xl text-black dark:text-gray-300 w-1/12">
+                                            <button className="hover:bg-gray-400/50 dark:hover:bg-gray-600/50 p-2 rounded-full">
+                                                <AiOutlineEdit />
+                                            </button>
+                                            <button className="text-red-600 hover:bg-gray-400/50 dark:hover:bg-gray-600/50 p-2 rounded-full">
+                                                <AiOutlineCloseCircle />
+                                            </button>
+                                        </div>
+                                    </div>
+                                ))}
+                                <button className="text-center py-2 w-full hover:bg-gray-400/50 rounded-lg mb-4">
+                                    <span className="text-gray-700 dark:text-gray-300 font-semibold">
+                                        Add Item +
+                                    </span>
                                 </button>
                             </div>
                         </div>
-                    ))}
-                    <button className="text-center py-2 w-full hover:bg-gray-400/50 rounded-lg mb-4">
-                        <span className="text-gray-700 dark:text-gray-300 font-semibold">Add Item +</span>
-                    </button>
-                </div>
-            </div>
-        </div>
+                        <div className="flex  mt-4 border-2 border-gray-600 dark:border-gray-500 bg-white dark:bg-secondary-dark h-[500px] w-full rounded-lg drop-shadow-xl overflow-y-auto">
+                            Profile
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <div className="flex justify-between p-4 gap-4 font-display">
+                        <div className="flex border-2 border-gray-600 dark:border-gray-500 bg-white dark:bg-secondary-dark h-[82vh] w-2/3 rounded-lg drop-shadow-xl">
+                            Profile
+                        </div>
+                        <div className="flex-row border-2 border-gray-600 dark:border-gray-500 bg-white dark:bg-secondary-dark h-[82vh] w-1/3 rounded-lg drop-shadow-xl overflow-y-auto scroll smooth">
+                            <div className="flex py-4 px-6 text-black dark:text-gray-300 text-lg font-bold">
+                                <h2>Upcoming:</h2>
+                            </div>
+                            <div className="flex-row px-6">
+                                {items.map((item) => (
+                                    <div
+                                        key={item.index}
+                                        className="flex-col justify-between w-[100%] p-4 mb-4 bg-main dark:bg-main-dark border-2 border-gray-600 dark:border-gray-500 rounded-xl drop-shadow-lg">
+                                        <div className="flex-row">
+                                            <span className="text-black dark:text-gray-300 text-md font-semibold w-11/12">
+                                                {item.title}
+                                                {" -"}
+                                            </span>
+                                            <span className="text-gray-700 dark:text-gray-400 text-sm line-clamp-2 mt-2 w-11/12">
+                                                {item.desc}
+                                            </span>
+                                            <div className="flex justify-between items-center mt-2 w-11/12">
+                                                <div className="">
+                                                    <span className="text-gray-700 dark:text-white text-sm italic font-semibold bg-yellow-300/50 p-1 rounded-md">
+                                                        {item.date}
+                                                    </span>
+                                                    <span className="text-gray-700 py-1 dark:text-gray-300 font-extrabold text-sm pl-2">
+                                                        |
+                                                    </span>
+                                                    <span className="px-2 py-1 text-red-600 text-sm font-semibold">
+                                                        {
+                                                            priority[
+                                                                item.priority
+                                                            ]
+                                                        }
+                                                    </span>
+                                                </div>
+                                                <div className="flex">
+                                                    {item.tags.map((tag) => (
+                                                        <div
+                                                            key={tag.title}
+                                                            className="mx-1 px-1 rounded-md"
+                                                            style={{
+                                                                backgroundColor:
+                                                                    tag.color,
+                                                            }}>
+                                                            <span className="text-white text-xs">
+                                                                {tag.title}
+                                                            </span>
+                                                        </div>
+                                                    ))}
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="grid absolute top-0 right-2 h-full place-content-evenly text-xl text-black dark:text-gray-300 w-1/12">
+                                            <button className="hover:bg-gray-400/50 dark:hover:bg-gray-600/50 p-2 rounded-full">
+                                                <AiOutlineEdit />
+                                            </button>
+                                            <button className="text-red-600 hover:bg-gray-400/50 dark:hover:bg-gray-600/50 p-2 rounded-full">
+                                                <AiOutlineCloseCircle />
+                                            </button>
+                                        </div>
+                                    </div>
+                                ))}
+                                <button className="text-center py-2 w-full hover:bg-gray-400/50 rounded-lg mb-4">
+                                    <span className="text-gray-700 dark:text-gray-300 font-semibold">
+                                        Add Item +
+                                    </span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            )}
+        </>
     );
 };
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
+
+import axios from "axios";
+
+import { basePath } from "../contexts/BuildPath";
 
 const Register = () => {
     const { currentColor } = useStateContext();
 
-    const message = "";
+    const [message, setMessage] = useState("");
+
+    const firstNameRef = useRef();
+    const lastNameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passConfRef = useRef();
+
+    const doRegister = (event) => {
+        event.preventDefault();
+        
+        if (passwordRef.current.value !== passConfRef.current.value) {
+            setMessage('Passwords must match');
+            return
+        }
+
+        axios.defaults.baseURL = basePath();
+        axios.post("/api/user/register", {
+            firstName: firstNameRef.current.value,
+            lastName: lastNameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        })
+        .then((res) => {
+            if (res.status === 201) {
+                setMessage('Registration Successful');
+            }
+        })
+        .catch((err) => {
+            if (err.response.status === 400) {
+                setMessage('Email already in use');
+            }
+            else {
+                setMessage('Unknown Error Occured');
+            }
+        });
+    };
 
     return (
         <div className="flex place-items-center justify-center mt-24">
-            <form className="flex-row justify-center border-2 border-black dark:border-white bg-white dark:bg-secondary-dark h-[625px] w-[400px] rounded-lg drop-shadow-xl p-4">
+            <form
+                onSubmit={doRegister}
+                className="flex-row justify-center border-2 border-black dark:border-white bg-white dark:bg-secondary-dark h-[625px] w-[400px] rounded-lg drop-shadow-xl p-4">
                 <div className="flex-row text-center mt-4">
                     <h2 className="text-black dark:text-white text-xl font-bold">
                         Welcome to Kalvin!
@@ -26,6 +68,8 @@ const Register = () => {
                             className="px-2 py-1 border-b-2 border-black dark:border-white bg-transparent w-full focus:outline-none focus:bg-gray-500/25 rounded-t-sm"
                             type="text"
                             placeholder="> First Name"
+                            ref={firstNameRef}
+                            id="firstName"
                         />
                     </label>
                     <label className="block dark:text-white my-4 px-4 w-full">
@@ -34,6 +78,8 @@ const Register = () => {
                             className="px-2 py-1 border-b-2 border-black dark:border-white bg-transparent w-full focus:outline-none focus:bg-gray-500/25 rounded-t-sm"
                             type="text"
                             placeholder="> Last Name"
+                            ref={lastNameRef}
+                            id="lastName"
                         />
                     </label>
                     <label className="block dark:text-white my-4 px-4 w-full">
@@ -42,6 +88,8 @@ const Register = () => {
                             className="px-2 py-1 border-b-2 border-black dark:border-white bg-transparent w-full focus:outline-none focus:bg-gray-500/25 rounded-t-sm"
                             type="text"
                             placeholder="> Email"
+                            ref={emailRef}
+                            id='email'
                         />
                     </label>
                     <label className="block dark:text-white my-4 px-4 w-full">
@@ -50,6 +98,8 @@ const Register = () => {
                             className="px-2 py-1 border-b-2 border-black dark:border-white bg-transparent w-full focus:outline-none focus:bg-gray-500/25 rounded-t-sm"
                             type="password"
                             placeholder="> Password"
+                            ref={passwordRef}
+                            id='password'
                         />
                     </label>
                     <label className="block dark:text-white my-4 px-4 w-full">
@@ -58,6 +108,8 @@ const Register = () => {
                             className="px-2 py-1 border-b-2 border-black dark:border-white bg-transparent w-full focus:outline-none focus:bg-gray-500/25 rounded-t-sm"
                             type="password"
                             placeholder="> Confirm Password"
+                            ref={passConfRef}
+                            id='passConf'
                         />
                     </label>
                 </div>
@@ -65,12 +117,21 @@ const Register = () => {
                     <button
                         type="submit"
                         className="block text-white px-2 py-2 rounded-md w-full mx-4 mt-2 outline outline-0 outline-offset-2 hover:outline-2"
+                        onClick={doRegister}
                         style={{
                             backgroundColor: currentColor,
                             outlineColor: currentColor,
                         }}>
                         Register
                     </button>
+                </div>
+                <div className="flex justify-center">
+                    <Link
+                        className="underline text-blue-400 hover:text-blue-600 mt-4"
+                        to={"/"}
+                        onClick={() => setMessage("")}>
+                        Back
+                    </Link>
                 </div>
             </form>
         </div>
